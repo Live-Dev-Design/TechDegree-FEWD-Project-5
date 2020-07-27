fa54f2188858ffb5224be321fe72ed4be5149970
Ryan Brecher <52445175+Live-Dev-Design@users.noreply.github.com>
2020-07-27 17:57:22 +0100

    cleaning up errors from validation, adding design styles

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
+
 /* search box */
+
 document.getElementById('search').addEventListener("keyup", imageSearch);
 
 function imageSearch() {
     const input = document.getElementById('search').value.toUpperCase();
     const images = document.querySelectorAll('.inner-wrapper a');
-    for (i = 0; i < images.length; i++) {
-
-        const imageCaption = document.getElementsByTagName('a')[i].getAttribute('title').toUpperCase();
-        const indexSearch = imageCaption.indexOf(input);
-
+    for (i = 0; i < images.length; i++) {                                               
+        const imageCaption = document.getElementsByTagName('a')[i].getAttribute('title').toUpperCase(); 
+        const indexSearch = imageCaption.indexOf(input);                               
         if (indexSearch > -1) {
             $(document.getElementsByTagName('a')[i]).fadeIn();
         } else {

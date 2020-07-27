bfb50bc56b3a7c1694ae2ebf23ed179a718f1319
Ryan Brecher <52445175+Live-Dev-Design@users.noreply.github.com>
2020-07-27 16:33:21 +0100

    edited js and css properties

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-const search = new Filter('search', 'data-caption')
+/* search box */
+document.getElementById('search').addEventListener("keyup", imageSearch);
+
+function imageSearch() {
+    const input = document.getElementById('search').value.toUpperCase();
+    const images = document.querySelectorAll('.inner-wrapper a');
+    for (i = 0; i < images.length; i++) {
+
+        const imageCaption = document.getElementsByTagName('a')[i].getAttribute('title').toUpperCase();
+        const indexSearch = imageCaption.indexOf(input);
+
+        if (indexSearch > -1) {
+            $(document.getElementsByTagName('a')[i]).fadeIn();
+        } else {
+            $(document.getElementsByTagName('a')[i]).fadeOut();
+        }
+    }
+}
